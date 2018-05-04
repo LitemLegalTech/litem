84ac46f44a3864b3b8ef784b5f30a5a398751c1a
Alex <alexjeffcott@gmail.com>
2018-05-04 17:15:06 +0200

    changed to chevron

--- src/components/styled-components/btn-scroll-down.js
+++ src/components/styled-components/btn-scroll-down.js
@@ -1,23 +1,34 @@
 import styled from 'styled-components';
-//content: '\\2B9F';
 
-const BtnScrollDown = styled.div`
+const arrow1 = `
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: auto;
+  margin-bottom: 20px;
+  height: 25px;
+  width: 25px;
+	border: 1px solid #3cc;
+	border-width: 0 2px 2px 0;`;
+
+const arrow2 = `
   position: fixed;
   bottom: 4px;
   left: 50%;
-  margin-left: -26px;
-  cursor: pointer;
+  margin-left: -20px;
   border: none;
   background-color: rgba(0, 0, 0, 0);
-  &:after {
-    content: '';
-    width: 0;
-    height: 0;
-    border-left: 20px solid transparent;
-    border-right: 20px solid transparent;
-    border-top: 20px solid ${props => props.theme.colors.color1};
-  }
+    &:after {
+      content: '';
+      width: 0;
+      height: 0;
+      border-left: 20px solid transparent;
+      border-right: 20px solid transparent;
+      border-top: 20px solid #3cc
+    }`;
 
+const BtnScrollDown = styled.div`
+  cursor: pointer;
+  ${arrow1};
   animation: bounce 4s infinite;
   @keyframes bounce {
     0%,
@@ -25,15 +36,15 @@ const BtnScrollDown = styled.div`
     50%,
     80%,
     100% {
-      transform: translateY(0);
+      transform: translateY(0) rotate(45deg);
       opacity: 0.9;
     }
     40% {
-      transform: translateY(-10px);
+      transform: translateY(-10px) rotate(45deg);
       opacity: 0.4;
     }
     60% {
-      transform: translateY(-4px);
+      transform: translateY(-4px) rotate(45deg);
       opacity: 0.9;
     }
   }
