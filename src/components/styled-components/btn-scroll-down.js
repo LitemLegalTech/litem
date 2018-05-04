9565635d94b431a5c79f6d08bd303809cf5e68d8
Alex <alexjeffcott@gmail.com>
2018-05-04 15:36:45 +0200

    centered css arrowhead

--- src/components/styled-components/btn-scroll-down.js
+++ src/components/styled-components/btn-scroll-down.js
@@ -3,12 +3,13 @@ import styled from 'styled-components';
 
 const BtnScrollDown = styled.button`
   position: fixed;
-  bottom: 15px;
-  left: 49%;
+  bottom: 10px;
+  left: 50%;
+  margin-left: -26px;
   cursor: pointer;
   border: none;
   background-color: rgba(0, 0, 0, 0);
-  &:before {
+  &:after {
     content: '';
     width: 0;
     height: 0;
